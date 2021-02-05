2c46ddb0dc437384e215b61ff35752fe7b7332ee
DanielQA123 <DAhuchogu@QA.com>
2021-02-05 10:28:07 +0000

    Added config package

---
+++ src/main/java/com/qa/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.qa.config;
+
+import java.time.LocalTime;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public String message() {
+		return "helloWorld";
+	}
+
+	@Bean
+	public String serverStart() {
+		return LocalTime.now().toString();
+	}
+
+}
