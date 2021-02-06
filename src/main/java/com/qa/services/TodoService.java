d130b9bc23c633724f999233aa20d4d6b1e576b6
DanielQA123 <DAhuchogu@QA.com>
2021-02-06 00:12:11 +0000

    added model mapper

--- src/main/java/com/qa/services/TodoService.java
+++ src/main/java/com/qa/services/TodoService.java
@@ -1,27 +1,35 @@
 package com.qa.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.qa.persistance.domain.Todo;
+import com.qa.persistance.dtos.TodoDTO;
 import com.qa.persistance.repos.TodoRepo;
 
 @Service
 public class TodoService {
 
 	private TodoRepo repo;
+	private ModelMapper mapper;
 
 	@Autowired
-	public TodoService(TodoRepo repo) {
+	public TodoService(TodoRepo repo, ModelMapper mapper) {
 		super();
 		this.repo = repo;
+		this.mapper = mapper;
 	}
 
-	//POST => CREATE:
-	
-	public Todo createTodo(Todo todo) {
+	private TodoDTO map(Todo model) {
+		return this.mapper.map(model, TodoDTO.class);
+	}
+
+	// POST => CREATE:
+
+	public TodoDTO createTodo(Todo todo) {
 		return this.repo.save(todo);
 	}
 
