b6e296b6935c3d39a36b27797df1ba5098c788e1
DanielQA123 <DAhuchogu@QA.com>
2021-02-08 20:47:07 +0000

    checked todoSystem classes work

--- src/main/java/com/qa/rest/TodoSystemController.java
+++ src/main/java/com/qa/rest/TodoSystemController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.persistance.domain.Todo;
+import com.qa.persistance.domain.TodoSystemDomain;
 import com.qa.persistance.dtos.TodoDTO;
+import com.qa.persistance.dtos.TodoSystemDTO;
 import com.qa.services.TodoSystemService;
 import com.qa.services.TodoService;
 
@@ -32,9 +34,6 @@ public class TodoSystemController {
 		this.service = service;
 	}
 
-//    private Long id = 0L;
-//	private List<Todo> todoList = new ArrayList<>();
-
 	@GetMapping("/helloWorld")
 	public String helloWorld() {
 		return "Hello world";
@@ -42,62 +41,36 @@ public class TodoSystemController {
 
 	// GET
 
-//	@GetMapping("/readAll")
-//	public List<Todo> readAll() {
-//		return todoList;
-//	}
-
 	@GetMapping("/readAll")
-	public ResponseEntity<List<TodoDTO>> readAll() {
-		return new ResponseEntity<List<TodoDTO>>(this.service.readAll(), HttpStatus.OK);
+	public ResponseEntity<List<TodoSystemDTO>> readAll() {
+		return new ResponseEntity<List<TodoSystemDTO>>(this.service.readAll(), HttpStatus.OK);
 	}
 
-//	@GetMapping("/read/{id}")
-//	public Todo readTodo(@PathVariable("id") Long id) {
-//		return todoList.get(id.intValue());
-//	}
 
 	@GetMapping("/read/{id}")
-	public ResponseEntity<TodoDTO> readTodo(@PathVariable("id") Long id) {
-		return new ResponseEntity<TodoDTO>(this.service.readTodo(id), HttpStatus.ACCEPTED);
+	public ResponseEntity<TodoSystemDTO> readTodo(@PathVariable("id") Long id) {
+		return new ResponseEntity<TodoSystemDTO>(this.service.readTodo(id), HttpStatus.ACCEPTED);
 	}
 
 	// POST= CREATE
 
-//	@PostMapping("/create")
-//	public boolean createTodo(@RequestBody Todo todo) {
-//		todo.setId(id);
-//		id++;
-//		
-//		return todoList.add(todo);
-//	}
 
 	@PostMapping("/create")
-	public ResponseEntity<TodoDTO> createTodo(@RequestBody Todo todo) {
-		return new ResponseEntity<TodoDTO>(this.service.createTodo(todo), HttpStatus.CREATED);
+	public ResponseEntity<TodoSystemDTO> createTodo(@RequestBody TodoSystemDomain model) {
+		return new ResponseEntity<TodoSystemDTO>(this.service.createTodo(model), HttpStatus.CREATED);
 	}
 
 	// PUT = UPDATE
-//	@PutMapping("/update/{id}")
-//	public Todo updateTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
-//		this.todoList.remove(id.intValue());
-//		this.todoList.add(id.intValue(), todo);
-//		return this.todoList.get(id.intValue());
-//	}
 
 	@PutMapping("/update/{id}")
-	public ResponseEntity <TodoDTO> updateTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
-		return new ResponseEntity<TodoDTO>(this.service.updateTodo(id, todo), HttpStatus.ACCEPTED);
+	public ResponseEntity <TodoSystemDTO> updateTodo(@PathVariable("id") Long id, @RequestBody TodoSystemDomain model) {
+		return new ResponseEntity<TodoSystemDTO>(this.service.updateTodo(id, model), HttpStatus.ACCEPTED);
 	}
 
 	// DELETE
-//	@DeleteMapping("/delete/{id}")
-//	public Todo deleteTodo(@PathVariable("id") Long id) {
-//		return todoList.remove(id.intValue());
-//	}
 
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<Object> deleteTodo(@PathVariable("id") Long id) {
+	public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
 		return this.service.delete(id) ? 
 				new ResponseEntity<>(HttpStatus.NO_CONTENT): 
 				new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
