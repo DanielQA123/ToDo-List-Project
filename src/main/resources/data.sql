2eddc49b5312a52d52eb109c5e795626e62e26bd
DanielQA123 <DAhuchogu@QA.com>
2021-02-09 00:00:20 +0000

    added DB relationship

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,15 @@
-INSERT INTO TODO_DOMAIN(DATE, DESCRIPTION, FINISH, TITLE) VALUES ('2020-09-11 15:45:15', 'complete project work', 'true', 'Assignment');
-INSERT INTO TODO_DOMAIN(DATE, DESCRIPTION, FINISH, TITLE) VALUES ('2020-12-21 12:15:15', 'Make Xmas dinner', 'false', 'Cook');
-INSERT INTO TODO_DOMAIN(DATE, DESCRIPTION, FINISH, TITLE) VALUES ('2021-01-10 15:45:15', 'Vist London Family', 'true', 'Family');
-INSERT INTO TODO_DOMAIN(DATE, DESCRIPTION, FINISH, TITLE) VALUES ('2020-09-25 09:45:15', 'Daily Run', 'true', 'Exercise');
+INSERT INTO TODO_SYSTEM_DOMAIN(OBJECTIVE) 
+VALUES 
+('Complete This Month'),
+('Complete This Month'),
+('Complete This Week'),
+('Complete Today');
+
+
+
+INSERT INTO TODO_DOMAIN(DATE, DESCRIPTION, FINISH, TITLE, THE_TODO_ID) 
+VALUES 
+('2020-12-11 15:45:15', 'complete project work', 'true', 'Assignment', 1),
+('2020-12-21 12:15:15', 'Make Xmas dinner', 'false', 'Cook', 1),
+('2021-01-10 15:45:15', 'Vist London Family', 'true', 'Family', 3),
+('2021-01-12 15:45:15', 'Daily Run', 'true', 'Exercise', 4);
