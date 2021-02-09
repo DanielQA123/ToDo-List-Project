6ad01810f8773f569102127b001810b25b7e73cf
DanielQA123 <DAhuchogu@QA.com>
2021-02-09 16:30:30 +0000

    fixed date issue

--- src/test/java/com/qa/services/TodoServiceTest.java
+++ src/test/java/com/qa/services/TodoServiceTest.java
@@ -1,15 +1,37 @@
 package com.qa.services;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.sql.Date;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.qa.persistance.domain.TodoDomain;
+import com.qa.persistance.domain.TodoSystemDomain;
+import com.qa.persistance.repos.TodoRepo;
 
 @SpringBootTest
 public class TodoServiceTest {
-	
-	//MY CRUD TEST
+
+	@MockBean
+	private ModelMapper mockedMapper;
+
+	@MockBean
+	private TodoRepo mockedRepo;
+
+	@Autowired
+	private TodoService service;
+
+	// MY CRUD TEST
 	@Test
 	public void createTodo() {
+	
+		TodoDomain testTodo = new TodoDomain(1L,"Cook","Dinner for today", Date.valueOf("2020-09-11"), true, null);
+		
+		Mockito.when(this.mockedRepo.save(Mockito.any(TodoDomain.class))).thenReturn(testTodo);
 
 	}
 
