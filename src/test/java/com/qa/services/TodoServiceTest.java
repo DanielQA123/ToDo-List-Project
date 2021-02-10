4c986a93d8f5d2299bd4fe87644fcbbd0cd0ff04
DanielQA123 <DAhuchogu@QA.com>
2021-02-10 10:40:15 +0000

    adding code for test class

--- src/test/java/com/qa/services/TodoServiceTest.java
+++ src/test/java/com/qa/services/TodoServiceTest.java
@@ -60,16 +60,22 @@ public class TodoServiceTest {
 	@Test
 	public void updateTodo() {
 		
-		TodoDomain testTodo = new TodoDomain(1L, "Assignment", "complete project work", Date.valueOf("2020-12-11"),
-				true, null);
-		TodoDTO testDTO = this.mockedMapper.map(testTodo, TodoDTO.class);
-		TodoDTO result = this.service.updateTodo(1L, testTodo);
-		
-		Mockito.when(this.mockedRepo.findById(testTodo.getId())).thenReturn(Optional.of(testTodo));
-		Mockito.when(this.mockedRepo.save(Mockito.any(TodoDomain.class))).thenReturn(testTodo);
-		Assertions.assertThat(result).isEqualTo(testDTO);
+		TodoDomain savedTodo = mockedRepo.save(new TodoDomain(1L, "Assignment", "finish project", Date.valueOf("2020-12-11"),
+				true, null));
+		Assertions.assertThat(savedTodo.getId()).isGreaterThan(0);
+		TodoDTO testDTO = this.mockedMapper.map(savedTodo, TodoDTO.class);
+		TodoDTO result = this.service.updateTodo(1L, savedTodo);
 		Mockito.verify(this.mockedRepo, Mockito.times(1)).save(Mockito.any(TodoDomain.class));
 		Mockito.verify(this.mockedRepo, Mockito.times(1)).findById(1L);
+		
+//		TodoDTO testDTO = this.mockedMapper.map(savedTodo, TodoDTO.class);
+//		TodoDTO result = this.service.updateTodo(1L, savedTodo);
+//		
+//		Mockito.when(this.mockedRepo.findById(testTodo.getId())).thenReturn(Optional.of(testTodo));
+//		Mockito.when(this.mockedRepo.save(Mockito.any(TodoDomain.class))).thenReturn(testTodo);
+//		Assertions.assertThat(result).isEqualTo(testDTO);
+//		Mockito.verify(this.mockedRepo, Mockito.times(1)).save(Mockito.any(TodoDomain.class));
+//		Mockito.verify(this.mockedRepo, Mockito.times(1)).findById(1L);
 		
 	}
 
--- src/test/java/com/qa/services/TodoSystemServiceTest.java
+++ src/test/java/com/qa/services/TodoSystemServiceTest.java
@@ -1,11 +1,20 @@
 package com.qa.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.qa.persistance.domain.TodoDomain;
+import com.qa.persistance.domain.TodoSystemDomain;
+import com.qa.persistance.dtos.TodoSystemDTO;
 import com.qa.persistance.repos.TodoSystemRepo;
 
 @SpringBootTest
@@ -13,26 +22,38 @@ public class TodoSystemServiceTest {
 
 	@MockBean
 	private ModelMapper mockedMapper;
-	
+
 	@MockBean
 	private TodoSystemRepo mockedRepo;
-	
+
 	@Autowired
 	private TodoSystemService service;
-	
+
 	@Test
-	public void createTodo() {	
+	public void createTodo() {
+		TodoSystemDomain testSystem = new TodoSystemDomain(1L, "Complete This Month", null);
+		Mockito.when(this.mockedRepo.save(Mockito.any(TodoSystemDomain.class))).thenReturn(testSystem);
+		TodoSystemDTO result = this.service.createTodo(testSystem);
+		Assertions.assertThat(result).isEqualTo(this.mockedMapper.map(testSystem, TodoSystemDTO.class));
+		Mockito.verify(this.mockedRepo, Mockito.times(1)).save(Mockito.any(TodoSystemDomain.class));
 	}
-	
-	
+
 	@Test
 	public void readAll() {
 	}
 
 	@Test
 	public void readTodo() {
-	}
 	
+		TodoSystemDomain testSystem = new TodoSystemDomain(1L, "Complete This Month", null);
+		Mockito.when(this.mockedRepo.findById(testSystem.getId())).thenReturn(Optional.of(testSystem));
+		TodoSystemDTO result = this.service.readTodo(1L);
+		TodoSystemDTO testDTO = this.mockedMapper.map(testSystem, TodoSystemDTO.class);
+		Assertions.assertThat(result).isEqualTo(testDTO);
+		Mockito.verify(this.mockedRepo, Mockito.times(1)).save(Mockito.any(TodoSystemDomain.class));
+	
+	}
+
 	@Test
 	public void updateTodo() {
 	}
