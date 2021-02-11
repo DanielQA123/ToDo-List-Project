be4080eb481dcf791ad426d9a37d880769e68463
DanielQA123 <DAhuchogu@QA.com>
2021-02-11 21:52:05 +0000

    TodoSystemDTO test

--- src/test/java/com/qa/persistance/dtos/TodoSystemDTOTest.java
+++ src/test/java/com/qa/persistance/dtos/TodoSystemDTOTest.java
@@ -1,5 +1,61 @@
 package com.qa.persistance.dtos;
 
-public class TodoSystemDTOTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TodoSystemDTOTest {
+	
+	public static TodoSystemDTO testDTO;
+	
+	@BeforeAll
+	public static void setUpTest() {
+		testDTO = new TodoSystemDTO(1L, "Complete This Month", null);
+	}
+	
+	@BeforeEach
+	public void testBefore() {
+		testDTO = new TodoSystemDTO(1L, "Complete This Week", null);
+	}
+
+	@Test
+	public void getSetIdTest() {
+		testDTO.setId(2L);
+		Long id = testDTO.getId();
+		assertEquals(2L, id);
+	}
+
+	@Test
+	public void getObjective() {
+		testDTO.getObjective();
+		assertEquals(testDTO.getObjective(), "Complete This Week");
+	}
+
+	@Test
+	public void setObjective() {
+		String objective = testDTO.getObjective();
+		testDTO.setObjective("Complete This Week");
+		assertEquals("Complete This Week", objective);
+	
+	}
+
+	@Test
+	public void getTodoList() {
+		
+	}
+
+	@Test
+	public void setTodoList() {
+	
+	}
+	
+	@Test
+	public void emptyConstructorTest() {
+		
+	}
+	
 }
