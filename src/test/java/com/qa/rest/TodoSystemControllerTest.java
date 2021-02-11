d0fc3c49e2654b25b63038902dcf111ba40dcae2
DanielQA123 <DAhuchogu@QA.com>
2021-02-11 12:19:09 +0000

    JUNIT testing create and read method

--- src/test/java/com/qa/rest/TodoSystemControllerTest.java
+++ src/test/java/com/qa/rest/TodoSystemControllerTest.java
@@ -1,5 +1,7 @@
 package com.qa.rest;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -45,18 +47,34 @@ public class TodoSystemControllerTest {
 	public void readAll() {
 		// RESOURCES:
 		TodoSystemDTO testDTO = new TodoSystemDTO(1L, "Complete This Month", null);
+		List<TodoSystemDTO> listDTO = List.of(testDTO);
 		
 		// RULES:
+		Mockito.when(this.service.readAll()).thenReturn(listDTO);
+		
 		// ACTION:
+		ResponseEntity<List<TodoSystemDTO>> result = this.controller.readAll();
+		
 		// ASSERTIONS:
+		Assertions.assertThat(result).isEqualTo(new ResponseEntity<List<TodoSystemDTO>>(listDTO, HttpStatus.OK));
+		Mockito.verify(this.service, Mockito.times(1)).readAll();
 	}
 
 	@Test
 	public void readTodo() {
 		// RESOURCES:
+		TodoSystemDomain testSystem = new TodoSystemDomain(1L, "Complete This Month", null);
+		TodoSystemDTO testDTO = new TodoSystemDTO(1L, "Complete This Month", null);
+		
 		// RULES:
+		Mockito.when(this.service.readTodo(1L)).thenReturn(testDTO);
+		
 		// ACTION:
+		ResponseEntity<TodoSystemDTO> result = this.controller.readTodo(1L);
+		
 		// ASSERTIONS:
+		Assertions.assertThat(result).isEqualTo(new ResponseEntity<TodoSystemDTO>(testDTO, HttpStatus.ACCEPTED));
+		Mockito.verify(this.service, Mockito.times(1)).readTodo(1L);
 	}
 
 	@Test
