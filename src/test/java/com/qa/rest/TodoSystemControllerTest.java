18cdefd3ed94d234d43caf312bfc8d8a06678385
DanielQA123 <DAhuchogu@QA.com>
2021-02-11 12:12:29 +0000

    JUNIT testing

--- src/test/java/com/qa/rest/TodoSystemControllerTest.java
+++ src/test/java/com/qa/rest/TodoSystemControllerTest.java
@@ -1,15 +1,51 @@
 package com.qa.rest;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.qa.persistance.domain.TodoSystemDomain;
+import com.qa.persistance.dtos.TodoDTO;
+import com.qa.persistance.dtos.TodoSystemDTO;
+import com.qa.services.TodoSystemService;
 
 @SpringBootTest
 public class TodoSystemControllerTest {
 	
+	@MockBean
+	private TodoSystemService service;
+	
+	@Autowired
+	private TodoSystemController controller;
+	
+	@Test
+	public void createTodo() {
+		// RESOURCES:
+		TodoSystemDomain testSystem = new TodoSystemDomain(1L, "Complete This Month", null);
+		TodoSystemDTO testDTO = new TodoSystemDTO(1L, "Complete This Month", null);
+		
+		// RULES:
+		Mockito.when(this.service.createTodo(testSystem)).thenReturn(testDTO);
+		
+		// ACTION:
+		ResponseEntity<TodoSystemDTO> result = this.controller.createTodo(testSystem);
 
+		// ASSERTIONS:
+		Assertions.assertThat(result).isEqualTo(new ResponseEntity<TodoSystemDTO>(testDTO, HttpStatus.CREATED));
+		Mockito.verify(this.service, Mockito.times(1)).createTodo(testSystem);
+	}
+	
+	
 	@Test
 	public void readAll() {
 		// RESOURCES:
+		TodoSystemDTO testDTO = new TodoSystemDTO(1L, "Complete This Month", null);
+		
 		// RULES:
 		// ACTION:
 		// ASSERTIONS:
@@ -17,14 +53,6 @@ public class TodoSystemControllerTest {
 
 	@Test
 	public void readTodo() {
-		// RESOURCES:
-		// RULES:
-		// ACTION:
-		// ASSERTIONS:
-	}
-
-	@Test
-	public void createTodo() {
 		// RESOURCES:
 		// RULES:
 		// ACTION:
