725e26b663717308c8d677433d51e284871b3802
DanielQA123 <DAhuchogu@QA.com>
2021-02-11 12:01:32 +0000

    TodoSystem controller test classes

--- src/test/java/com/qa/rest/TodoSystemControllerTest.java
+++ src/test/java/com/qa/rest/TodoSystemControllerTest.java
@@ -1,5 +1,58 @@
 package com.qa.rest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class TodoSystemControllerTest {
+	
+
+	@Test
+	public void readAll() {
+		// RESOURCES:
+		// RULES:
+		// ACTION:
+		// ASSERTIONS:
+	}
+
+	@Test
+	public void readTodo() {
+		// RESOURCES:
+		// RULES:
+		// ACTION:
+		// ASSERTIONS:
+	}
+
+	@Test
+	public void createTodo() {
+		// RESOURCES:
+		// RULES:
+		// ACTION:
+		// ASSERTIONS:
+	}
+
+	@Test
+	public void updateTodo() {
+		// RESOURCES:
+		// RULES:
+		// ACTION:
+		// ASSERTIONS:
+	}
+
+	@Test
+	public void deleteSuccessful() {
+		// RESOURCES:
+		// RULES:
+		// ACTION:
+		// ASSERTIONS:
+	}
+
+	@Test
+	public void deleteUnsuccessful() {
+		// RESOURCES:
+		// RULES:
+		// ACTION:
+		// ASSERTIONS:
+	}
 
 }
