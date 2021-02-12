cf908db4f9f1abf2d2cb255f0f8ace46e9be6b15
DanielQA123 <DAhuchogu@QA.com>
2021-02-12 15:44:35 +0000

    create and read integration test

--- src/test/java/com/qa/rest/TodoControllerIntegrationTest.java
+++ src/test/java/com/qa/rest/TodoControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.qa.rest;
 
+import java.sql.Date;
+
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
@@ -21,55 +24,76 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.persistance.domain.TodoDomain;
 import com.qa.persistance.dtos.TodoDTO;
 
-@SpringBootTest (webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@Sql(scripts = {"classpath:data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = { "classpath:data-test.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles(profiles = "test")
 public class TodoControllerIntegrationTest {
-	
+
 	@Autowired
 	private MockMvc mock;
-	
+
 	@Autowired
 	private ModelMapper mapper;
-	
+
 	@Autowired
 	private ObjectMapper jsonifier;
-	
+
 	private final int ID = 1;
-	
+
 	private TodoDTO mapToDTO(TodoDomain model) {
 		return this.mapper.map(model, TodoDTO.class);
 	}
-	
+
 	@Test
 	public void createTodo() throws Exception {
-	}
+
+		//Resources:
+		TodoDomain contentBody = new TodoDomain(5L, "Shopping", "Buy Pjs", Date.valueOf("2021-01-11"),
+				true, null);
+		TodoDTO expectedResult = mapToDTO(contentBody);
+
+		// Set Up request:
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.POST,
+				"http://localhost:8080/todo/create").contentType(MediaType.APPLICATION_JSON).content(jsonifier.writeValueAsBytes(contentBody)).accept(MediaType.APPLICATION_JSON);
+
+		// Set up expectation:
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isCreated();
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
 	
+		// perform/action
+		this.mock.perform(mockRequest).andExpect(matchStatus);
+		this.mock.perform(mockRequest).andExpect(matchContent);
+
+	}
+
 	@Test
 	public void readAll() throws Exception {
 	}
 
 	@Test
 	public void readTodo() throws Exception {
-		
-		//Set Up request: 
-		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.GET, "http://localhost:8080/todo/read/" + ID);
-		
-		//Set up expectation: 
+		TodoDTO expectedResult = new TodoDTO(1L, "Assignment", "complete project work", Date.valueOf("2020-12-11"),
+				true);
+
+		// Set Up request:
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.GET,
+				"http://localhost:8080/todo/read/" + ID);
+
+		// Set up expectation:
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isAccepted();
-		
-		//perform
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+		// perform
 		this.mock.perform(mockRequest).andExpect(matchStatus);
-		
+		this.mock.perform(mockRequest).andExpect(matchContent);
 	}
 
 	@Test
-	public void updateTodo() throws Exception{
+	public void updateTodo() throws Exception {
 	}
 
 	@Test
-	public void deleteTodo() throws Exception{
+	public void deleteTodo() throws Exception {
 	}
 
 }
