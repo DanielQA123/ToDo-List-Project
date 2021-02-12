26e110acec44c9c5d5712761ef20875aa75cd40a
DanielQA123 <DAhuchogu@QA.com>
2021-02-12 14:14:08 +0000

    integration testing for read

--- src/test/java/com/qa/rest/TodoControllerIntegrationTest.java
+++ src/test/java/com/qa/rest/TodoControllerIntegrationTest.java
@@ -1,5 +1,64 @@
 package com.qa.rest;
 
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.persistance.domain.TodoDomain;
+import com.qa.persistance.dtos.TodoDTO;
+
+@SpringBootTest (webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles(profiles = "test")
 public class TodoControllerIntegrationTest {
+	
+	@Autowired
+	private MockMvc mock;
+	
+	@Autowired
+	private ModelMapper mapper;
+	
+	@Autowired
+	private ObjectMapper jsonifier;
+	
+	private final int ID = 1;
+	
+	private TodoDTO mapToDTO(TodoDomain model) {
+		return this.mapper.map(model, TodoDTO.class);
+	}
+	
+	@Test
+	public void createTodo() {
+	}
+	
+	@Test
+	public void readAll() {
+	}
+
+	@Test
+	public void readTodo() {
+		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.GET, "todo/read/" + ID);
+	}
+
+	@Test
+	public void updateTodo() {
+	}
+
+	@Test
+	public void deleteTodo() {
+	}
 
 }
